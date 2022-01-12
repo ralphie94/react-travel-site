804abd030ec9aad1a6eb4ea0362131bd90fa7fcf
Ralphie Celedon <ralphie94@pacbell.net>
2022-01-12 11:38:38 -0800

    created hero section in home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route path='/' element={<Home/>} />
         </Routes>
       </Router>
     </>
