a233fa577179c03340f5cbf1d2245b743624ab0e
Ralphie Celedon <ralphie94@pacbell.net>
2022-01-13 10:32:44 -0800

    created hero section in home page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,7 +9,13 @@ function Cards() {
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
-                        <CardItem />
+                        <CardItem src='images/img-9.jpg' text='Explore the hidden waterfall deep inside the Amazon Jungle' label='Adventure' path='/services' />
+                        <CardItem src='images/img-2.jpg' text='Travel through the islands of Bali in a private cruise' label='Luxury' path='/services' />
+                    </ul>
+                    <ul className='cards__items'>
+                        <CardItem src='images/img-3.jpg' text='Set sail in the Atlantic Ocean visiting uncharted waters' label='Mystery' path='/services' />
+                        <CardItem src='images/img-4.jpg' text='Experience football on top of the Himalayan Mountains' label='Adventure' path='/products' />
+                        <CardItem src='images/img-8.jpg' text='Ride through the Sahara Desert on a guided camel tour' label='Adrenaline' path='/sign-up' />
                     </ul>
                 </div>
             </div>
